eeacbfd2bcd567518e6614c57c432bf9fcc865d5
rubiyadav18 <73836805+rubiyadav18@users.noreply.github.com>
2020-12-07 22:37:25 -0800

    Add files via upload

---
+++ calculate_question_.py
@@ -0,0 +1,14 @@
+
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=(input("enter a number"))
+def fuction(a,b,c):
+    if c=="add":
+        print(a+b)
+    elif c=="substract":
+        print(a-b)
+    elif c=="multiply":
+        print(a*b)
+    elif c=="divide":
+        print(a/b)
+(fuction(a,b,c))
