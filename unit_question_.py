ca00e829547a144061215464e01b3974c1e31864
rubiyadav18 <73836805+rubiyadav18@users.noreply.github.com>
2020-12-13 20:01:11 -0800

    Add files via upload

---
+++ unit_question_.py
@@ -0,0 +1,13 @@
+unit=int(input("enter a unit bil"))
+if unit>=50 and unit >=50:
+    print(0.50*50+0.2)
+elif unit>=100 and unit<=200:
+    print(0.75*100+0.2)
+elif unit>=100 and unit<=200:
+    print(1.20*100+0.2)
+elif unit==250:
+    print(1.50*250+0.2)
+elif unit<=40:
+    print(1*unit)
+else:
+    print("no charges")
